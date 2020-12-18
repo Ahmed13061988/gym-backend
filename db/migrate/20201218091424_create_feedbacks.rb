93ba1a33b5ba4c7201a7e6bce26e10003eccf266
Ahmed <wolktheline@yahoo.com>
2020-12-18 02:16:03 -0700

    adding a feedback  model

--- db/migrate/20201218091424_create_feedbacks.rb
+++ db/migrate/20201218091424_create_feedbacks.rb
@@ -1,6 +1,8 @@
 class CreateFeedbacks < ActiveRecord::Migration[6.0]
   def change
     create_table :feedbacks do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :restaurant, null: false, foreign_key: true
       t.string :content
       t.string :rating
 
