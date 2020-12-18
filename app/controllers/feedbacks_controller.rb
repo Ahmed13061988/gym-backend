7fdf45dc822640417e1042224ebc8355530f2723
Ahmed <wolktheline@yahoo.com>
2020-12-18 02:17:01 -0700

    adding a feedback  controller

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,12 @@
 class FeedbacksController < ApplicationController
+    def create
+        review = logged_in_user.reviews.create(review_params)
+        render json: review
+      end
+    
+      private
+    
+      def review_params
+        params.permit(:restaurant_id, :content, :rating)
+      end
 end
